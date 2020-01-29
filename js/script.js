2228d371ce2e695dcc027e362dfeae2071878c39
Isaac Vongphakdy <IVongphakdy@gmail.com>
2020-01-29 12:48:07 -0800

    updated js functions

--- js/script.js
+++ js/script.js
@@ -7,7 +7,21 @@ let apiKey = '&appid=65c2a04c4c37cb19ae57a213f9dce232';
 fetch(apiUrl + apiKey)
     .then(response => response.json())
     .then(data => {
-        // const yolo = JSON.parse(data);
-        console.log(data.weather[0].main);
+        const condition = data.weather[0].main;
+        if (condition == "Thunderstorm") {
+            console.log("Heavy coat + umbrella");
+        } else if (condition == "Drizzle" || condition == "Rain") {
+            console.log("Sweater + umbrella");
+        } else if (condition == "Snow") {
+            console.log("Snow coat");
+        } else if (condition == "Smoke" || condition == "Dust" || condition == "Sand" || condition == "Ash") {
+            console.log("Face mask");
+        } else if (condition == "Clear") {
+            console.log("T shirt");
+        } else if (condition == "Clouds") {
+            console.log("Long sleeve");
+        } else {
+            console.log("Figure dis out");
+        }
     })
     .catch(err => console.log(err));
